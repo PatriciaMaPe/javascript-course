deb6306e3ed4ce2e5ca1dccbc496b51bed1aa191
PatriciaMaPe <martinperez.patricia@gmail.com>
2020-04-06 12:41:20 +0200

    Show use of variables

---
+++ variables.js
@@ -0,0 +1,11 @@
+var name;
+name = 'Patri';
+var age = 26;
+
+// var name = 'Patri', age = 26;
+
+console.log('Hi ' + name + ' - ' + age);
+
+//Javascript is a weak typed language
+age = '26' // -> change the number type to a text
+console.log('Hi ' + name + ' - ' + age);
